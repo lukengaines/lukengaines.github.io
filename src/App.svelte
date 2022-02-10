bf8d9a470be749c19e99a3e159d96843ca56dab1
kalebal <kaleblyda14@gmail.com>
2022-02-09 19:47:34 -0500

    setup svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<main>
+	<header>
+		<h1>Luke Gaines</h1>
+	</header>
+</main>
+
+<style>
+	main {
+		/* text-align: center; */
+		padding: 1em;
+		/* max-width: 240px; */
+		margin: 0 auto;
+		display: flex;
+	}
+	header {
+		display: flex;
+	}
+	h1 {
+		color: #ffebe4;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (max-width: 640px) {
+		main {
+			max-width: none;
+		}
+		h1 {
+		font-size: 3em;
+		}
+	}
+</style>
